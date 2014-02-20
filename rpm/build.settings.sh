8cc1f4fd83ad0147f4f90eb8d707341506d07470
Mutsutoshi Yoshimoto <mutsutoshi.yoshimoto@mixi.co.jp>
2014-02-20 17:43:34 +0900

    import from turnserver-3.2.2.7.tar.gz

---
+++ rpm/build.settings.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Common settings script.
+
+TURNVERSION=3.2.2.7
+BUILDDIR=~/rpmbuild
+ARCH=`uname -p`
+TURNSERVER_SVN_URL=http://rfc5766-turn-server.googlecode.com/svn
+TURNSERVER_SVN_URL_VER=branches/v3.2
+
+WGETOPTIONS="--no-check-certificate"
+RPMOPTIONS="-ivh --force"
+
+# DIRS
+
+mkdir -p ${BUILDDIR}
+mkdir -p ${BUILDDIR}/SOURCES
+mkdir -p ${BUILDDIR}/SPECS
+mkdir -p ${BUILDDIR}/RPMS
+mkdir -p ${BUILDDIR}/tmp
+
+# Common packs
+
+PACKS="make gcc redhat-rpm-config rpm-build doxygen openssl-devel svn"
+sudo yum -y install ${PACKS}
+ER=$?
+if ! [ ${ER} -eq 0 ] ; then
+    echo "Cannot install packages ${PACKS}"
+    exit -1
+fi
+
